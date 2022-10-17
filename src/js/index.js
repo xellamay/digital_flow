b93c096a5883214ff5d5bff63c08be0c489f5adb
Alena <xellatroy@gmail.com>
2022-10-17 17:06:01 +0300

    сделала меню

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  initMenu()
+};
+
+function initMenu() {
+  const btnMenuOpen = document.querySelector(".header__menu-button");
+  const btnMenuClose = document.querySelector(".menu__button");
+  const drawer = document.querySelector(".js-drawer");
+
+  btnMenuOpen.addEventListener("click", function() {
+    drawer.classList.add("drawer_open")
+  });
+
+  btnMenuClose.addEventListener("click", function() {
+    drawer.classList.remove("drawer_open")
+  });
+};
